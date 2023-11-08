ae9b3d4f7e40f8727d1f0afe3a1eeda5df12448c
ferdousalam007 <iamferdousalam007@gmail.com>
2023-11-08 12:37:11 +0600

    feat: counter app declare action types and action creator

--- react-redux-hooks-middlewares/react-redux/src/App.jsx
+++ react-redux-hooks-middlewares/react-redux/src/App.jsx
@@ -1,12 +1,33 @@
-import './App.css'
+import "./App.css";
 function App() {
-  
-
   return (
     <>
-     <center> <h1>hello react redux</h1></center>
+      <center>
+        {" "}
+        <h1>hello react redux</h1>
+      </center>
+      <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
+        {/* header */}
+        <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
+          Simple Counter Application
+        </h1>
+        {/* counters */}
+        <div className="max-w-md mx-auto mt-10 space-y-5">
+          <div className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
+            <div className="text-2xl font-semibold">0</div>
+            <div className="flex space-x-3">
+              <button className="bg-indigo-400 text-white px-3 py-2 rounded shadow">
+                Increment
+              </button>
+              <button className="bg-red-400 text-white px-3 py-2 rounded shadow">
+                Decrement
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
